ad78e4c8ceb8329776df29266e80a96c5554eb84
Monika <monikanimmana3366@gmail.com>
2026-02-10 11:11:22 +0530

    User Register was created in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -5,3 +5,21 @@ from rest_framework.decorators import api_view
 from .models import *
  
 # Create your views here.
+@api_view(['POST'])
+def Register(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    email = request.data.get('email')
+
+    if UserProfile.objects.filter(username=username).exists():
+        return Response({'error':'username already exists'},status=400)
+    
+    user = UserProfile.objects.create_user(
+        username = username,
+        password = password,
+        email = email,
+    )
+
+    return Response({'message': 'User is successfully created'})
+    
+    
