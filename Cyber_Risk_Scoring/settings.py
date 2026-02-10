1e15de78cb1e48b02764d25ff9fc1aa3e1019a96
Monika <monikanimmana3366@gmail.com>
2026-02-10 10:33:34 +0530

    Updated in settings.py

--- Cyber_Risk_Scoring/settings.py
+++ Cyber_Risk_Scoring/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
