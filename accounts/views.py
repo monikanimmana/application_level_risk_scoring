f94ebc160da3482fc26c6bf1746884eb5be14062
Monika <monikanimmana3366@gmail.com>
2026-02-10 10:52:22 +0530

    created views.py for UserProfile

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate , login
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import *
+ 
 # Create your views here.
