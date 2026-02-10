2c5166f89a9884f288d55ed2a90d40f1c0345091
Monika <monikanimmana3366@gmail.com>
2026-02-10 10:13:50 +0530

    Inital Setting Up Risk Scoring project

---
+++ accounts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class UserProfile(AbstractUser):
+        ROLE_CHOICES = (
+            ('user', 'Normal User'),
+            ('admin', 'Admin'),
+        )
+        STATUS_CHOICES = (
+                ('active','Active'),
+                ('restricted','Restricted'),
+                ('blocked','Blocked'),         
+        )
+        
+        role = models.CharField(max_length=10,choices=ROLE_CHOICES,default='user')
+        last_ip = models.GenericIPAddressField(null=True, blank=True)
+        last_device = models.TextField(null=True, blank=True)
+        risk_score = models.IntegerField(default=0)
+
+        def __str__(self):
+            return self.username
+
+
+
+
